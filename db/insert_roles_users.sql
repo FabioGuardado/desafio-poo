df2410552043e0e6fc666184015ec562832dc7d9
Fabio Guardado <fabio.guardado2012@gmail.com>
2024-10-27 18:01:13 -0600

    mover archivos de base de datos

---
+++ db/insert_roles_users.sql
@@ -0,0 +1,15 @@
+INSERT INTO `mediateca`.`rol`
+(`id`,
+`nombre`)
+VALUES
+(1,
+"administrador");
+SELECT * FROM mediateca.rol;
+
+INSERT INTO `mediateca`.`rol`
+(`id`,
+`nombre`)
+VALUES
+(2,
+"socio");
+SELECT * FROM mediateca.rol;
