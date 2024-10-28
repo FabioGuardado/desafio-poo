32c4d3a1fb32deb3c056e86b6dd41c0bbf44188a
Fabio Guardado <fabio.guardado2012@gmail.com>
2024-10-27 18:16:41 -0600

    actualizar sql para insertar usuario

--- db/insert_roles_users.sql
+++ db/insert_roles_users.sql
@@ -12,4 +12,24 @@ INSERT INTO `mediateca`.`rol`
 VALUES
 (2,
 "socio");
+
 SELECT * FROM mediateca.rol;
+
+alter table mediateca.usuario add column password varchar(50) after correo;
+
+INSERT INTO `mediateca`.`usuario`
+(`id`,
+`rol_id`,
+`nombre`,
+`apellido`,
+`correo`,
+`password`)
+VALUES
+(1,
+1,
+"Fabio",
+"Guardado",
+"fabio@mediateca.com",
+"mediateca123");
+
+select * from mediateca.usuario;
